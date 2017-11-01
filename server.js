9d16b3c2b98dc2bb95ca8842b402220166a9760a
Stephen Buchanan <stephen.buchanan@random.nu>
2017-11-01 11:43:10 +0100

    upload files with extension ok via multer

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
+const multer  = require('multer')
+const path = require('path')
 
-app.get('/', function (req, res) {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+     cb(null, 'uploads/')
+   },
+   filename: function (req, file, cb) {
+     cb(null, Date.now() + path.extname(file.originalname)) //Appending .jpg
+   }
+})
+
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
+})
+
+app.post('/upload', upload.single('sample'), (req, res, next) => {
+  console.log('got file upload', req.file);
+  // req.file is the `sample` file
+  // req.body will hold the text fields, if there were any
+  res.send(req.file);
 })
