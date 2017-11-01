ff9ff7ca0db4600518a9603d5b951f9029cd77d4
Stephen Buchanan <stephen.buchanan@random.nu>
2017-11-01 11:50:59 +0100

    moved server to server folder

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-     cb(null, 'uploads/')
+     cb(null, 'server/uploads/')
    },
    filename: function (req, file, cb) {
      cb(null, Date.now() + path.extname(file.originalname)) //Appending .jpg
